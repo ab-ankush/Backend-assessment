588115cd0dfd7601d7db1121e5dddf581a0fe4e4
ab-ankush <77474256+ab-ankush@users.noreply.github.com>
2023-06-10 15:29:53 +0530

    Update usersRoutes.js

--- server/routers/usersRoutes.js
+++ server/routers/usersRoutes.js
@@ -10,10 +10,19 @@ import verify from "../middleware/authenticate.js";
 
 const router = express.Router();
 
+// Api end point to get the user data
 router.get("/:id", getUser);
+
+// Api end point to ass a user 
 router.post("/register", addUser);
+
+// Api end point to edit the user data
 router.patch("/:id/edit", updateUser);
+
+// Api end point to reveal the user data
 router.get("/:id/reveal", verify, revealUser);
+
+// Api end point to open or close the user data
 router.put("/:id/switch", verify, switchUser);
 
 export default router;
